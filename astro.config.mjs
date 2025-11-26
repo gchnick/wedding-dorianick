847b08e5cdface8a7fadd63e464c1ecb245506c7
Nick Galán <gnick.alan@gmail.com>
2025-11-25 22:48:28 -0400

    chore: :wrench: Update Astro configuration to include Cloudflare adapter, database integration, sitemap generation, and path aliasing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,31 @@
 // @ts-check
+import path from "node:path";
+
 import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
+import cloudflare from "@astrojs/cloudflare";
+import db from "@astrojs/db";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.dorianick.com",
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
   },
-  integrations: [react()],
+  integrations: [react(), sitemap(), db()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+
+    imageService: "compile",
+  }),
+  output: "server",
 });
