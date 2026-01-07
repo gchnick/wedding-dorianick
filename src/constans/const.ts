3cd317b3875cdb8cd4bf67cd6b03c5a1da873169
Nick Galán <gnick.alan@gmail.com>
2026-01-07 19:53:51 -0400

    feat: :sparkles: Add María Gabriela Laya's image and update bridal party list in constants

--- src/constans/const.ts
+++ src/constans/const.ts
@@ -1,17 +1,17 @@
 import daiwyImage from "@/assets/daiwy.webp";
+import mariaLaya from "@/assets/maria-gabriela-laya.webp";
 import yoraimaImage from "@/assets/yoraima.webp";
 import yordalisImage from "@/assets/yordalis.webp";
 
-export const weddingDateStringWithDay = "Viernes, 30 de enero de 2026"
-export const weddingDateString = "30 de enero de 2026"
-export const weddingDate = new Date('2026-01-30T20:00:00Z');
-
+export const weddingDateStringWithDay = "Viernes, 30 de enero de 2026";
+export const weddingDateString = "30 de enero de 2026";
+export const weddingDate = new Date("2026-01-30T20:00:00Z");
 
 // Bridal Party
 export const Girls = [
   { name: "Yoraima Rámirez", role: "Amiga del novio", photo: yoraimaImage },
   { name: "Yordalis Benavides", role: "Amiga del novio", photo: yordalisImage },
-  { name: "Nayan Macheref", role: "Amiga de la novia" },
+  { name: "María Gabriela Laya", role: "Amiga de la novia", photo: mariaLaya },
 ];
 
 export const Boys = [
